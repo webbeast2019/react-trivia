c735c6f63e8ac33a4f13207b63b0b9eebca8d08d
yuvalbl <yuval.bar.levi@gmail.com>
2019-06-02 21:21:58 +0300

    add redux

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 interface IProps {
   text: string
   options: Array<string>,
   onNext: Function
+  numOfQuestions: number
 }
 
-const Question: React.FC<IProps> = ({text, options, onNext}) => {
+const Question: React.FC<IProps> = ({numOfQuestions, text, options, onNext}) => {
   function onClick(answerIndex: number) {
     onNext(answerIndex);
   }
@@ -21,6 +23,7 @@ const Question: React.FC<IProps> = ({text, options, onNext}) => {
 
   return (
     <div>
+      <h1>Data from redux store: {numOfQuestions}</h1>
       <h1>{text}</h1>
       <ul>
         {optionalAnswers}
@@ -29,4 +32,23 @@ const Question: React.FC<IProps> = ({text, options, onNext}) => {
   );
 };
 
-export default Question;
+
+const mapStateToProps = (state: any) => {
+  return {
+    numOfQuestions: state.length
+  }
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    onNext: (index:number) => {
+      const action = {
+        type: 'ADVANCE_ACTIVE_QUESTION',
+        payload: index
+      };
+      dispatch(action);
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Question);
---
+++ src/store/store.ts
@@ -0,0 +1,37 @@
+import {createStore} from 'redux'
+import {IQuestion} from '../models/IQuestion';
+
+const questions: Array<IQuestion> = [
+  {
+    id: 'q1',
+    text: 'The name of WHICH US state is thought to have been made up as the result of a hoax?',
+    options: ['Iowa', 'Minnesota', 'Idaho', 'Michigan'],
+    correctIndex: 2
+  },
+  {
+    id: 'q2',
+    text: 'The capital of Cuba is which of the following?',
+    options: ['Guantanamo Bay', 'Tijuana', 'San Jose', 'Havana'],
+    correctIndex: 3
+  },
+  {
+    id: 'q3',
+    text: 'Aside from Denmark, Danish is the official language of WHICH other country?',
+    options: ['Greenland', 'Germany', 'Iceland', 'Finland'],
+    correctIndex: 0
+  },
+  {
+    id: 'q4',
+    text: 'Aside from Denmark, Danish is the official language of WHICH other country?',
+    options: ['Greenland', 'Germany', 'Iceland', 'Finland'],
+    correctIndex: 0
+  },
+];
+
+const dataReducer = (state = questions, action: any) => {
+  return state;
+};
+
+export default createStore(
+  dataReducer,
+);
